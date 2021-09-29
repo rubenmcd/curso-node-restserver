0150f9fcab20bae7683f23b1dd4cb1597f7bf73d
Ruben Martin del Campo <rp.mcd@hotmail.com>
2021-09-28 19:31:38 -0500

    CRUD productos, categorias y busquedas

--- helpers/db-validators.js
+++ helpers/db-validators.js
@@ -1,3 +1,4 @@
+const { Categoria, Producto } = require("../models");
 const Role = require("../models/role");
 const Usuario = require("../models/usuario");
 
@@ -19,7 +20,21 @@ const emailExiste = async (correo = "") => {
 const existeUsuarioPorId = async (id) => {
   const existeUsuario = await Usuario.findById(id);
   if (!existeUsuario) {
-    throw new Error(`El id: ${id}, no existe`);
+    throw new Error(`El usuario con id: ${id}, no existe`);
+  }
+};
+
+const existeCategoriaPorId = async (id) => {
+  const existeCategoria = await Categoria.findById(id);
+  if (!existeCategoria) {
+    throw new Error(`La categoria con id: ${id} no existe`);
+  }
+};
+
+const existeProductoPorId = async (id) => {
+  const existeProducto = await Producto.findById(id);
+  if (!existeProducto) {
+    throw new Error(`El producto con id: ${id} no existe`);
   }
 };
 
@@ -27,4 +42,6 @@ module.exports = {
   esRolValido,
   emailExiste,
   existeUsuarioPorId,
+  existeCategoriaPorId,
+  existeProductoPorId,
 };
